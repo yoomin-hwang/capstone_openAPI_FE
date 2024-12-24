71dbb9404f7cb0bde641c06a217ca927d9e22227
Yoomin Hwang <yoominh3@gmail.com>
2024-12-25 03:15:57 +0900

    Feat: Header 기능만 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
 export const Header = () => {
     return (
         <div>
-            <div>header</div>
+            <div>
+                <Link to="/">
+                    지수모
+                </Link>
+                <div>
+                    <Link to="/">
+                        홈
+                    </Link>
+                    <Link to="/likes">
+                        즐겨찾기
+                    </Link>
+                </div>
+            </div>
             <Outlet/>
         </div>
     );
